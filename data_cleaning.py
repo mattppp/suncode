aaa044854e707301ea1672a9debb3273038788f3
JamesHan1008 <james@arceoanalytics.com>
2018-04-14 09:19:09 -0700

    a

--- data_cleaning.py
+++ data_cleaning.py
@@ -16,17 +16,19 @@ def read_as_dict(data_df, key):
     return d
 
 def add_cols(data_df, source_dict, key):
-    columns = data_df.columns.tolist()
+    data_new = data_df
+    columns = data_new.columns.tolist()
 
-    for i in range(len(data_df['JobId'])):
+    for i in range(len(data_new['JobId'])):
         
-        k = data_df[key][i]
-        print(str(k))
-        # for c in source_dict[k].keys():
-        #     if c not in columns:
-        #         data_df[c] = 0
-        #         columns.append([c])
-        #     data_df[c][i] = source_dict[k][c]
+        k = data_new.get_value(i, key)
+        for c in source_dict[k].keys():
+            if c not in columns:
+                data_new[c] = 0
+                columns.append([c])
+            data_new.set_value(i, c, source_dict[k][c])
+
+    return data_new
 
 data_installed = pd.read_csv('data/PV_installed_customer_details.csv', sep=',', encoding='ISO-8859-1')
 data_cancelled = pd.read_csv('data/PV_cancelled_customer_details.csv', sep=',', encoding='ISO-8859-1')
@@ -37,7 +39,7 @@ factors = [
     'EngineeringSoldAnnualkWh',
     'EnergyConsumption',
 #    'Region',
-#    'State',
+    'State',
 #    'Zip',
 #    'Latitude',
 #    'Longitude',
@@ -66,12 +68,15 @@ data_installed['Status'] = 1
 data_cancelled['Status'] = 0
 data_combined = data_installed.append(data_cancelled)
 
-# data_sunlight = pd.read_csv('data/Google Sunroof_Yearly_Sunlight_by_State.csv', sep=',', encoding='ISO-8859-1')
-# data_sunlight = read_as_dict(data_df=data_sunlight, key='State')
+data_sunlight = pd.read_csv('data/Google Sunroof_Yearly_Sunlight_by_State.csv', sep=',', encoding='ISO-8859-1')
+data_sunlight = read_as_dict(data_df=data_sunlight, key='State')
 
-# add_cols(data_df=data_combined, source_dict=data_sunlight, key='State')
+#TODO DELETE
+data_combined = data_combined.head(n=100)
 
-# print(data_combined.head(n=5))
+data_combined = add_cols(data_df=data_combined, source_dict=data_sunlight, key='State')
+
+print(data_combined.head(n=5))
 
 # for row in range(len(data_combined['JobId'])):
 #     
@@ -80,5 +85,5 @@ data_combined = data_installed.append(data_cancelled)
 
 # print(data_combined.head(n=5))
 
-data_out = data_combined.head(n=100).append(data_combined.tail(n=100))
-data_out.to_csv('data_combined.csv',sep=',')
+#data_out = data_combined.head(n=100).append(data_combined.tail(n=100))
+#data_out.to_csv('data_combined.csv',sep=',')
