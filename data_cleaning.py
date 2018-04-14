b024f257aaa141f0199d0ed2ce23ae0bc3123b60
JamesHan1008 <james@arceoanalytics.com>
2018-04-14 10:46:00 -0700

    combine columns

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def read_as_dict(data_df, key):
     keys = data_df[key]
@@ -15,17 +16,17 @@ def read_as_dict(data_df, key):
 
     return d
 
-def add_cols(data_df, source_dict, key):
+def add_cols(data_df, source_dict, key, new_columns):
     data_new = data_df
     columns = data_new.columns.tolist()
+
     sample_key = data_new.get_value(0, key)
     if not isinstance(sample_key, str):
         sample_key = sample_key[0]
 
-    for c in source_dict[sample_key].keys():
-        if c not in columns:
-            data_new[c] = 0
-            columns.append([c])
+    for c in new_columns:
+        data_new[c] = np.nan
+        columns.append([c])
 
     for i in range(len(data_new['JobId'])):
 
@@ -33,11 +34,16 @@ def add_cols(data_df, source_dict, key):
             print(i)
 
         k = data_new.get_value(i, key)
-        if not isinstance(k, str):
+        if not isinstance(k, str) and not isinstance(k, float) and not isinstance(k, int):
             k = k[0]
 
-        for c in source_dict[k].keys():
-            data_new.set_value(i, c, source_dict[k][c])
+        try:
+            if key == 'Zip':
+                k = int(k)
+            for c in source_dict[k].keys():
+                data_new.set_value(i, c, source_dict[k][c])
+        except:
+            continue
 
     return data_new
 
@@ -51,7 +57,7 @@ factors = [
     'EnergyConsumption',
 #    'Region',
     'State',
-#    'Zip',
+    'Zip',
 #    'Latitude',
 #    'Longitude',
 #    'AHJ',
@@ -79,29 +85,39 @@ data_installed['Status'] = 1
 data_cancelled['Status'] = 0
 data_combined = data_installed.append(data_cancelled)
 
+#TODO DELETE
+#data_combined = data_combined.head(n=len(data_combined))
+data_sample = data_combined.head(n=10000).append(data_combined.tail(n=10000), ignore_index=True)
+
+
 data_sunlight = pd.read_csv('data/Google Sunroof_Yearly_Sunlight_by_State.csv', sep=',', encoding='ISO-8859-1', low_memory=False)
 data_sunlight = read_as_dict(data_df=data_sunlight, key='State')
+data_sample = add_cols(data_df=data_sample, source_dict=data_sunlight, key='State', new_columns=[
+    'yearly_sunlight_kwh_kw_threshold_avg',
+    'yearly_sunlight_kwh_n',
+    'yearly_sunlight_kwh_s',
+    'yearly_sunlight_kwh_e',
+    'yearly_sunlight_kwh_w',
+    'yearly_sunlight_kwh_f',
+    'yearly_sunlight_kwh_median',
+    'yearly_sunlight_kwh_total',
+])
 
-# #TODO DELETE
-# #data_combined = data_combined.head(n=len(data_combined))
-# data_combined = data_combined.head(n=50000)
+data_insolation = pd.read_csv('data/Insolation_by_Zip.csv', sep=',', encoding='ISO-8859-1', low_memory=False)
+data_insolation = read_as_dict(data_df=data_insolation, key='Zip')
+data_sample = add_cols(data_df=data_sample, source_dict=data_insolation, key='Zip', new_columns=[
+    'Insolation',
+])
 
-# print(len(data_combined))
-
-# data_combined = add_cols(data_df=data_combined, source_dict=data_sunlight, key='State')
-
-# print(data_combined.head(n=5))
+# data_census = pd.read_csv('data/ZIP_incomepop.csv', sep=',', encoding='ISO-8859-1', low_memory=False)
+# data_census = read_as_dict(data_df=data_census, key='Zip')
+# data_sample = add_cols(data_df=data_sample, source_dict=data_census, key='Zip')
 
 
-# for row in range(len(data_combined['JobId'])):
-#     
-#     state_row = list(data_sunlight['State']).index(state)
-#     data_combined['yearly_sunlight_kwh_kw_threshold_avg'][row] = data_sunlight['yearly_sunlight_kwh_kw_threshold_avg'][state_row]
-
-# print(data_combined.head(n=5))
+print(data_sample.head(n=5))
 
 
 # Printing
-data_out = data_combined.head(n=100).append(data_combined.tail(n=100))
-data_out.to_csv('data_combined_truncated.csv', sep=',')
-data_combined.to_csv('data_combined.csv',sep=',')
+# data_sample = data_combined.head(n=100).append(data_combined.tail(n=100))
+# data_sample.to_csv('data_combined_truncated.csv', sep=',')
+# data_combined.to_csv('data_combined.csv',sep=',')
