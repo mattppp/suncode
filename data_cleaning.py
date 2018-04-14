fdb6fb2ef9b8205f70bcb9f0d53aa277388063be
JamesHan1008 <james@arceoanalytics.com>
2018-04-14 09:47:15 -0700

    two datasets

--- data_cleaning.py
+++ data_cleaning.py
@@ -15,23 +15,24 @@ def read_as_dict(data_df, key):
 
     return d
 
-# def add_cols(data_df, source_dict, key):
-#     data_new = data_df
-#     columns = data_new.columns.tolist()
-#     for c in source_dict[k].keys():
-#         if c not in columns:
-#             data_new[c] = 0
+def add_cols(data_df, source_dict, key):
+    data_new = data_df
+    columns = data_new.columns.tolist()
+    sample_key = data_new.get_value(0, key)
+    #if sample_key
 
-#     for i in range(len(data_new['JobId'])):
-        
-#         k = data_new.get_value(i, key)
-#         for c in source_dict[k].keys():
-            
-                
-#                 columns.append([c])
-#             data_new.set_value(i, c, source_dict[k][c])
+    print(sample_key)
+    for c in source_dict[sample_key].keys():
+        if c not in columns:
+            data_new[c] = 0
+            columns.append([c])
 
-#     return data_new
+    for i in range(len(data_new['JobId'])):
+        k = data_new.get_value(i, key)
+        for c in source_dict[k].keys():
+            data_new.set_value(i, c, source_dict[k][c])
+
+    return data_new
 
 data_installed = pd.read_csv('data/PV_installed_customer_details.csv', sep=',', encoding='ISO-8859-1')
 data_cancelled = pd.read_csv('data/PV_cancelled_customer_details.csv', sep=',', encoding='ISO-8859-1')
@@ -71,19 +72,16 @@ data_installed['Status'] = 1
 data_cancelled['Status'] = 0
 data_combined = data_installed.append(data_cancelled)
 
-# data_sunlight = pd.read_csv('data/Google Sunroof_Yearly_Sunlight_by_State.csv', sep=',', encoding='ISO-8859-1')
-# data_sunlight = read_as_dict(data_df=data_sunlight, key='State')
+data_sunlight = pd.read_csv('data/Google Sunroof_Yearly_Sunlight_by_State.csv', sep=',', encoding='ISO-8859-1')
+data_sunlight = read_as_dict(data_df=data_sunlight, key='State')
 
-# #TODO DELETE
-# data_combined = data_combined.head(n=100)
+#TODO DELETE
+#data_combined = data_combined.head(n=len(data_combined))
+data_combined = data_combined.head(n=9714)
 
-# data_combined = add_cols(data_df=data_combined, source_dict=data_sunlight, key='State')
+data_combined = add_cols(data_df=data_combined, source_dict=data_sunlight, key='State')
 
-# print(data_combined.head(n=5))
-
-
-
-
+#print(data_combined.head(n=5))
 
 
 # for row in range(len(data_combined['JobId'])):
@@ -93,5 +91,6 @@ data_combined = data_installed.append(data_cancelled)
 
 # print(data_combined.head(n=5))
 
-#data_out = data_combined.head(n=100).append(data_combined.tail(n=100))
+data_out = data_combined.head(n=100).append(data_combined.tail(n=100))
+data_out.to_csv('data_combined_truncated.csv', sep=',')
 data_combined.to_csv('data_combined.csv',sep=',')
